c5c71119e31eefd126821c13b828e5ac98441ea5
Zzzeeke <113009427+Zzzeeke@users.noreply.github.com>
2022-09-26 20:31:18 +0800

    Add files via upload

---
+++ SchoolLab-main/push.bat
@@ -0,0 +1,7 @@
+@Echo off
+git remote set-url origin https://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@github.com/Zzzeeke/SchoolLab.git
+pause
+git add .
+git commit -m "Automatic push"
+git push
+pause
